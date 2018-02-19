ffc29db0729db0d27a252a9347edd13292ed33ba
AKogut <andruhakogut@gmail.com>
2018-02-19 11:29:44 +0200

    Labs

---
+++ Lab21/src/Float.java
@@ -0,0 +1,3 @@
+public class Float {
+    public static float var;
+}
---
+++ Lab21/src/PassObject.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class PassObject {
+
+    public static void f(Float z){
+        Random rand  = new Random();
+        z.var = rand.nextFloat();
+    }
+}
