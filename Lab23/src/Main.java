486a10973b944b220612e320a86cef219321cab8
AKogut <andruhakogut@gmail.com>
2018-02-21 10:39:15 +0200

    Labs

---
+++ Lab23/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[]args){
+        for (int i = 1; i < 11; i++){
+            switch (i){
+                case 1: System.out.println("Case 1: " + i);
+                    break;
+                case 2: System.out.println("Case 2: " + i);
+                    break;
+                case 3: System.out.println("Case 3: " + i);
+                    break;
+                case 4: System.out.println("Case 4: " + i);
+                    break;
+                case 5: System.out.println("Case 5: " + i);
+                    break;
+                case 6: System.out.println("Case 6: " + i);
+                    break;
+                case 7: System.out.println("Case 7: " + i);
+                    break;
+                case 8: System.out.println("Case 8: " + i);
+                    break;
+                case 9: System.out.println("Case 9: " + i);
+                    break;
+                case 10: System.out.println("Case 10: " + i);
+                    break;
+            }
+        }
+    }
+}
